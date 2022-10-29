8732bf79ece08ca8cdcb2bba923e79b36f204e11
CrypticHushane <oshanewilliams99@outlook.com>
2022-10-29 03:05:54 -0500

    Initial Commit

---
+++ RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by Oshane Williams on 29/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
